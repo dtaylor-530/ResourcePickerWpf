f6f5f554b52a10e74a8d97355eb107d002410750
dtaylor-530 <d.taylor530@gmail.com>
2019-01-20 10:01:44 +0000

    initial

---
+++ LoadResourceWpf.Demo/frmLoadResources.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Markup;
+using System.IO;
+
+namespace WPFLoadResources_CS
+{
+  /// <summary>
+  /// Interaction logic for frmLoadResources.xaml
+  /// </summary>
+  public partial class frmLoadResources : Window
+  {
+    public frmLoadResources()
+    {
+      InitializeComponent();
+    }
+
+
+  }
+}
